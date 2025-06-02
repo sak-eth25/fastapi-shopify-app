4d56990ef064135841885dd074b97833df3864df
Saketh Naidu Dasari <160517410+sak-eth25@users.noreply.github.com>
2025-06-02 15:53:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://yourstore.myshopify.com"],  # or ["*"] for testing
+    allow_origins=["*"],  # or ["*"] for testing
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
